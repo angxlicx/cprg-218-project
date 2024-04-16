41a4155ca0845a68daba6f6f19ea51b570435457
angxlicx <kelly.hoang@edu.sait.ca>
2024-04-16 12:11:06 -0600

    the start

---
+++ javascript/java.js
@@ -0,0 +1,28 @@
+/*accordion*/
+var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      
+      this.classList.toggle("active");
+  
+      
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+        this.querySelector('i').classList.remove('fa-minus');
+        this.querySelector('i').classList.add('fa-plus');
+      } else {
+        panel.style.display = "block";
+        this.querySelector('i').classList.remove('fa-plus');
+        this.querySelector('i').classList.add('fa-minus');
+      }
+    });
+  }
+
+/*footer copyright*/
+document.addEventListener('DOMContentLoaded', function() {
+  var currentYear = new Date().getFullYear();
+  document.getElementById('copyright').innerHTML = '&copy; ' + currentYear + ' Kelly Hoang. All rights reserved.';
+});
